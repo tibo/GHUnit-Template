858379530f08bff59910f18235e9d89c0658db48
Thibaut LE LEVIER <thibaut@lelevier.fr>
2011-06-14 23:03:08 +0200

    ajout projet

---
+++ GHUnit Test Case.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,37 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import <GHUnitIOS/GHUnit.h>
+//#import <OCMock/OCMock.h>
+ 
+@interface ___FILEBASENAMEASIDENTIFIER___ : GHTestCase { }
+@end
+ 
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+//Method called before each tests
+- (void) setUp
+{
+    
+}
+
+//Method called before after each tests
+- (void) tearDown
+{
+    
+} 
+ 
+- (void)testSimplePass {
+	// Another test
+}
+ 
+- (void)testSimpleFail {
+	GHAssertTrue(NO, nil);
+}
+ 
+@end
